1e357a0f8d29d22e5809994bc294d0d2cbff1c30
ItonRomero <jaysonromero1218@gmail.com>
2023-02-17 13:59:34 +0800

    added deploy.sh

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -34,7 +34,7 @@ const Footer = () => {
 
         <div className="w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3F3E45]">
             <p className="font-poppins font-normal text-center text-[18px] leading-[27px] text-white">
-                2022 HooBank. All Rigths Reserved.
+                2023 Iton. All Rigths Reserved.
             </p>
 
             <div className="flex flex-row md:mt-0 mt-6">
@@ -43,7 +43,7 @@ const Footer = () => {
                         key={social.id}
                         src={social.icon}
                         alt={social.id}
-                        className={`w-[21px] h-[21px] object-contain ${index !== socialMedia.length - 1 ? 'mr-6' : 'mr-0'}`}
+                        className={`w-[21px] h-[21px] object-contain hover:fill-red-500 ${index !== socialMedia.length - 1 ? 'mr-6' : 'mr-0'}`}
                     />
                 ))}
             </div>
